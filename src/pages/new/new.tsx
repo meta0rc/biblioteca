4c6cc9ec2a79a9d8250562922fef21411c4b12cd
gabrielelia5ilva <gabriel.silva@digitalpages.com.br>
2022-11-15 15:42:08 -0300

    fix

--- src/pages/new/new.tsx
+++ src/pages/new/new.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Scanner } from "../../components/scanner/Scanner";
-
+import './style.css'
 export const NewLocation = () => {
   const [camera, setCamera] = useState(false);
   const [result, setResult] = useState(null);
@@ -10,7 +10,7 @@ export const NewLocation = () => {
   };
   return (
     <div className="AppCanvas">
-      <p style={{color: ''}}>{result ? result : "Scanning..."}</p>
+      <p style={{color: '#fff', fontSize: '30px'}}>{result ? result : "Scanning..."}</p>
       <button onClick={() => setCamera(!camera)}>
         {camera ? "Stop" : "Start"}
       </button>
